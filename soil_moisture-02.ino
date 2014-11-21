e536445a6ef7181e30d49dabdaba62ae4845039d
shkelley <UncleRedBeard@users.noreply.github.com>
2014-11-21 12:41:08 -0600

    initial commit

---
+++ soil_moisture-02.ino
@@ -0,0 +1,23 @@
+/*
+ * File:    soil_moisture.ino
+ * Author:  Uncle RedBeard
+ * Date:    19-NOV-2014
+ * Desc:    Testing Sunkee Soil Hygrometer
+ */
+ 
+ int soil = 0;
+ unsigned long waitTime = 10000;    //how long to pause - 5s to start
+ unsigned long previousMillis = 0;
+ 
+ void setup(){
+   Serial.begin(9600);    //initialize serial comm at 9600 bps
+ }
+ 
+ void loop(){
+   
+   delay(1000);
+   int sensorValue = analogRead(A0);
+   
+   Serial.println(sensorValue);
+     
+ }
---
+++ soil_moisture.ino
@@ -0,0 +1,34 @@
+/*
+ * File:    soil_moisture.ino
+ * Author:  Uncle RedBeard
+ * Date:    19-NOV-2014
+ * Desc:    Testing Sunkee Soil Hygrometer
+ */
+ 
+ int soil = 0;
+ unsigned long waitTime = 2000;
+ unsigned long previousMillis = 0;
+ 
+ void setup(){
+   Serial.begin(9600);    //initialize serial comm at 9600 bps
+ }
+ 
+ void loop(){
+   
+   if ((unsigned long)(millis() - previousMillis) >= waitTime) {
+     //read input val on A0 - analog pin
+     int sensorValue = analogRead(A0);
+     //sensorValue = constrain(sensorValue, 485, 1023);
+   
+     //raw sensor value
+     Serial.println(sensorValue);
+   
+     //map the value to a percentage
+     soil = map(sensorValue, 375, 1023, 100, 0);
+     
+     //print the soil moisture percentage
+     Serial.print(soil);
+     Serial.println("%");
+   }
+     
+ }
